8139ec83a3b0619055f91a4de91edcef53406f95
wuzhaoqing325 <1586607026@qq.com>
2020-02-06 17:46:18 +0800

    product

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,10 +1,23 @@
 // http://happymmall.com/product/list.do?keyword=1
 
-var _mm = require('util/mm.js');
-
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var navSide = require('page/common/nav-side/index.js');
-navSide.init({
-	name:'user-center'
+require('util/slider/index.js');
+var templateBanner = require('./banner.string');
+var _mm = require('util/mm.js');
+
+$(function(){
+	// 渲染banner的html
+	var bannerHtml = _mm.renderHtml(templateBanner);
+	$('.banner-con').html(bannerHtml);
+	// 初始化banner
+	var $slider = $('.banner').unslider({
+		dots:true
+	});
+	// 前一张和后一张操作的事件绑定
+	$('banner-con .banner-arrow').click(function(){
+		var forword = $(this).hasClass('prev') ? 'prev' : 'next';
+		$slider.data('unslider')[forword]();
+	});
 });
---
+++ src/page/list/index.js
@@ -0,0 +1,105 @@
+require('./index.css');
+require('page/common/nav/index.js');
+require('page/common/header/index.js');
+require('util/slider/index.js');
+var templateIndex = require('./index.string');
+var _mm = require('util/mm.js');
+var _product = require('service/product-service.js');
+var Pagination = require('util/pagination/index.js');
+
+var page = {
+	data:{
+		listParam:{
+			keyword:_mm.getUrlParam('keyword') || '',
+			categoryId:_mm.getUrlParam('categoryId') || '',
+			orderBy:_mm.getUrlParam('orderBy') || 'default',
+			pageNum:_mm.getUrlParam('pageNum') || 1,
+			pageSize:_mm.getUrlParam('pageSize') || 20
+		}
+	},
+	init:function(){
+		this.onLoad();
+		this.bindEvent();
+	},
+	onLoad:function(){
+		this.loadList();
+	},
+	bindEvent:function(){
+		var _this = this;
+		// 排序的点击事件
+		$('.sort-item').click(function(){
+			var $this = $(this);
+			_this.data.listParam.pageNum = 1;
+			// 点击默认排序
+			if($this.data('type') === 'default'){
+				if($this.hasClass('active')){
+					return;
+				}
+				else{
+					$this.addClass('active').siblings('.sort-item')
+					.removeClass('active asc desc');
+					_this.data.listParam.orderBy = 'default';
+				}
+			}
+			// 点击价格排序
+			else if($this.data('type') === 'price'){
+				// active class处理
+				$this.addClass('active').siblings('.sort-item')
+					.removeClass('active asc desc');
+				// 升序降序的处理
+				// 如果是降序，就变为升序
+				if(!$this.hasClass('asc')){
+					$this.addClass('asc').removeClass('desc');
+					_this.data.listParam.orderBy = 'price-asc';
+				}else{
+					$this.addClass('desc').removeClass('asc');
+					_this.data.listParam.orderBy = 'price-desc';
+				}
+			}
+			// 重新加载列表
+			_this.loadList();
+		});
+	},
+	// 加载list数据
+	loadList:function(){
+		var _this = this,
+			listHtml = '',
+			listParam = this.data.listParam,
+			$pListCon = $('.p-list-con');
+		$pListCon.html('<div class="loading"></div>');
+		// 删除参数中不必要的字段
+		listParam.categoryId ? (delete listParam.keyword) : (delete listParam.categoryId);
+		// 请求接口
+		_product.getProductList(listParam, function(res){
+			listHtml = _mm.renderHtml(templateIndex, {
+				list : res.list
+			});
+			$pListCon.html(listHtml);
+			_this.loadPagination({
+				hasPreviousPage : res.hasPreviousPage,
+				prePage : res.prePage,
+				hasNextPage : res.hasNextPage,
+				nextPage : res.nextPage,
+				pageNum: res.pageNum,
+				pages: res.pages
+			}, function(errMsg){
+				_mm.errorTips(errMsg);
+			});
+		})
+	},
+	// 加载分页信息
+	loadPagination:function(pageInfo){
+		var _this = this;
+		this.pagination ? '' : (this.pagination = new Pagination());
+		this.pagination.render($.extend({}, pageInfo, {
+			container: $('.pagination'),
+			onSelectPage:function(pageNum){
+				_this.data.listParam.pageNum = pageNum;
+				_this.loadList();
+			}
+		}));
+	}
+};
+$(function(){
+	page.init();
+})
