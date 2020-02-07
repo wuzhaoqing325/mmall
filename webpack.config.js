044197f150dd4b52ca8c56349b077e32617a5682
wuzhaoqing325 <1586607026@qq.com>
2020-02-07 17:15:42 +0800

    payment

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ var config = {
         'list'                          : ['./src/page/list/index.js'],
         'detail'                        : ['./src/page/detail/index.js'],
         'cart'                          : ['./src/page/cart/index.js'],
+        'order-confirm'                 : ['./src/page/order-confirm/index.js'],
+        'order-list'                    : ['./src/page/order-list/index.js'],
+        'order-detail'                  : ['./src/page/order-detail/index.js'],
+        'payment'                       : ['./src/page/payment/index.js'],
         'user-login'                    : ['./src/page/user-login/index.js'],
         'user-register'                 : ['./src/page/user-register/index.js'],
         'user-pass-reset'               : ['./src/page/user-pass-reset/index.js'],
@@ -32,6 +36,7 @@ var config = {
         'user-center-update'            : ['./src/page/user-center-update/index.js'],
         'user-pass-update'              : ['./src/page/user-pass-update/index.js'],
         'result'                        : ['./src/page/result/index.js'],
+        'about'                         : ['./src/page/about/index.js'],
         
     },
     output: {
@@ -71,6 +76,10 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表页')),
         new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情页')),
         new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
@@ -78,7 +87,8 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-center', '个人中心')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
-        new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果'))
+        new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMALL'))
         
     ]
 };
